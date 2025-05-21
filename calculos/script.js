1444165d63b71c5928f6ef3820274e368c52bd96
LUAN VICTOR PANIAGUA CASTRO <luan.vpcastro@senacsp.edu.br>
2025-05-21 17:33:52 -0300

    script

---
+++ calculos/script.js
@@ -0,0 +1,28 @@
+const a = 5;
+const b = 2;
+const c = 3;
+const divisao = a / b;
+const mult = a * b * c;
+const restodadivisao = a % b;
+const exponenciacao = a ** b;
+
+// soma
+console.log(a+b);
+// Subtração
+console.log(a-b);
+// Multiplicação
+console.log(a*b);
+// Divisão
+console.log(a/b);
+// Teste do Luan
+console.log(mult);
+
+// Conteúdo que a professora passou
+console.log(restodadivisao,("\n"),exponenciacao); 
+
+//Precedência 
+console.log((1+1)*4**2)
+
+console.log((2-2)*(3*3))
+
+console.log(3*2/3)
---
+++ variavel/script.js
@@ -0,0 +1,31 @@
+// Comentário em uma linha 
+
+/* comentário
+
+em várias
+
+linhas */
+
+// Exibe comando na tela
+console.log(2);
+
+console.log(typeof 2.5);
+
+// Declarando constantes
+const meunumero = 10;
+console.log(meunumero);
+
+// Declarando variáveis
+let novonumero = 100;
+novonumero = 150;
+console.log(novonumero);
+
+// string
+console.log("Olá mundo!");
+
+// contagem de string
+const nome = "Luan Paniagua";
+console.log(nome.length);
+
+// Pular uma lunha na string
+console.log("Pular uma linha para mostrar o resultado \nR$125,00");
