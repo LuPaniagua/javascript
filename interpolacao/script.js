682d09b42c48de64a2561772738ff07fa7e46922
Luan Paniagua <luanvictor.castro11@gmail.com>
2025-05-28 13:42:38 -0300

    Add files via upload

---
+++ interpolacao/script.js
@@ -0,0 +1,13 @@
+// Interpolando uma string, um número e uma expressão
+const primeironome = "João";
+console.log(`Olá, ${primeironome}!`);
+
+const idade = 30;
+console.log(`João tem ${idade} anos.`);
+
+const resultado = 2 + 2;
+console.log(`O resultado é ${resultado}`);
+
+const primo = 1399;
+console.log(25245 * primo);
+console.log(`Olá! Meu número favorito é ${primo}`);
---
+++ operadoreslogicos/script.js
@@ -0,0 +1,13 @@
+// Operadores Lógicos
+const condicaoA = true;
+const condicaoB = true;
+
+// Comparação AND eu uso este símbolo &&
+console.log(condicaoA && condicaoB);
+
+// Comparação OR eu uso este símbolo ||
+console.log(condicaoA || condicaoB);
+
+// Se eu quiser negal algo eu usa a exclamação na frente do boleano
+const condicaoC = !true;
+console.log(condicaoA && condicaoC);
